4b6979c2539c2b84de90d44c66c9c0cd6bee4834
kushal <kushalhts00@gmail.com>
2025-01-18 04:25:56 +0530

    save

--- index.js
+++ index.js
@@ -22,5 +22,7 @@ app.get('/',(req,res)=>{
 
 //app.use('/api', tutorRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
