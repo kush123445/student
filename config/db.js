ee2c2e00644f149deaf95527d83a3d69da7ccccd
kushal <kushalhts00@gmail.com>
2025-01-18 13:59:54 +0530

    s

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     console.log('MongoDB Connected...');
   } catch (error) {
     console.error('Database connection failed:', error.message);
-    process.exit(1);
+   
   }
 };
 
